a4cd09063264a6756fc41330ab3004b77bc0c7bc
James Dowell <james.s.dowell@gmail.com>
2018-09-11 21:03:03 +0000

    ex 9

--- lib/store.rb
+++ lib/store.rb
@@ -3,6 +3,13 @@ class Store < ActiveRecord::Base
   validates :name, length: { minimum: 3}
   validates :annual_revenue, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 } 
   validate :apparel
+  before_destroy :check
+
+  def check 
+    return true if employees.size < 2
+    errors.add :base, "Cannot delete"
+    throw(:abort)
+  end
 
   def apparel
     if mens_apparel == 'false' || womens_apparel == 'false'
